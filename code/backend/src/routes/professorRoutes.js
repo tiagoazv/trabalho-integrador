5ac1a8e111468b7d289486369adb1bd9fe18b365
TIAGO ALEX GONÇALVES DE AZEVEDO <tgoncalvesazevedo@gmail.com>
2024-07-01 17:30:18 -0300

    update professorRoutes

--- code/backend/src/routes/professorRoutes.js
+++ code/backend/src/routes/professorRoutes.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.get('/', professorCtrl.getProfessor);
 router.get('/:id', professorCtrl.getProfessorById);
+router.get('/login/:username', professorCtrl.getProfessorLogin);
 router.post('/', professorCtrl.createProfessor);
 router.put('/:id', professorCtrl.updateProfessor);
 router.delete('/:id', professorCtrl.deleteProfessor);
